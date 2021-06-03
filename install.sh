c7285ed9c526e52040989529618c846b793a8c20
gitzhangzhao <zhangzhao@ihep.ac.cn>
2021-06-03 10:27:43 +0800

    2021年 06月 03日 星期四 10:27:43 CST

--- install.sh
+++ install.sh
@@ -14,33 +14,33 @@ function check_prog() {
 }
 
 function deploy_dotfiles() {
-sudo stow --no-fold --targe /etc apt
-sudo stow --no-fold --targe /etc network
-sudo stow --no-fold --targe /etc systemd
-sudo stow --no-fold --targe /etc samba
-stow --no-fold --target "$HOME" Xresources
-stow --no-fold --target "$HOME" bin
-stow --no-fold --target "$HOME" compton
-stow --no-fold --target "$HOME" cron
-stow --no-fold --target "$HOME" dunst
-stow --no-fold --target "$HOME" flameshot
-stow --no-fold --target "$HOME" fonts
-stow --no-fold --target "$HOME" gdb
-stow --no-fold --target "$HOME" git
-stow --no-fold --target "$HOME" goldendict
-stow --no-fold --target "$HOME" i3
-stow --no-fold --target "$HOME" mpv
-stow --no-fold --target "$HOME" nitrogen
-stow --no-fold --target "$HOME" picom
-stow --no-fold --target "$HOME" polybar
-stow --no-fold --target "$HOME" qterminal
-stow --no-fold --target "$HOME" qv2ray
-stow --no-fold --target "$HOME" scripts
-stow --no-fold --target "$HOME" ssh
-stow --no-fold --target "$HOME" vim
-stow --no-fold --target "$HOME" wallpaper
-stow --no-fold --target "$HOME" zsh
-stow --no-fold --target "$HOME" sogoupinyin
+sudo stow --no-fold --restow --targe /etc apt
+sudo stow --no-fold --restow --targe /etc network
+sudo stow --no-fold --restow --targe /etc systemd
+sudo stow --no-fold --restow --targe /etc samba
+stow --no-fold --restow --target "$HOME" Xresources
+stow --no-fold --restow --target "$HOME" bin
+stow --no-fold --restow --target "$HOME" compton
+stow --no-fold --restow --target "$HOME" cron
+stow --no-fold --restow --target "$HOME" dunst
+stow --no-fold --restow --target "$HOME" flameshot
+stow --no-fold --restow --target "$HOME" fonts
+stow --no-fold --restow --target "$HOME" gdb
+stow --no-fold --restow --target "$HOME" git
+stow --no-fold --restow --target "$HOME" goldendict
+stow --no-fold --restow --target "$HOME" i3
+stow --no-fold --restow --target "$HOME" mpv
+stow --no-fold --restow --target "$HOME" nitrogen
+stow --no-fold --restow --target "$HOME" picom
+stow --no-fold --restow --target "$HOME" polybar
+stow --no-fold --restow --target "$HOME" qterminal
+stow --no-fold --restow --target "$HOME" qv2ray
+stow --no-fold --restow --target "$HOME" scripts
+stow --no-fold --restow --target "$HOME" ssh
+stow --no-fold --restow --target "$HOME" vim
+stow --no-fold --restow --target "$HOME" wallpaper
+stow --no-fold --restow --target "$HOME" zsh
+stow --no-fold --restow --target "$HOME" sogoupinyin
 }
 
 check_prog stow
