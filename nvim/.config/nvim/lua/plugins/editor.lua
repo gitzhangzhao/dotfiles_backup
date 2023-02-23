52631260a83c7de3286c22425ae814a5eb5a5855
gitzhangzhao <gitzhangzhao@gmail.com>
2023-02-24 00:00:02 +0800

    2023年 02月 24日 星期五 00:00:02 CST

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -386,4 +386,19 @@ return {
         end
     },
 
+    {
+        'mawkler/modicator.nvim',
+        init = function()
+            -- These are required for Modicator to work
+            vim.o.cursorline = true
+            vim.o.number = true
+            vim.o.termguicolors = true
+        end,
+        config = function()
+            require('modicator').setup({
+                -- ...
+            })
+        end,
+    }
+
 }
